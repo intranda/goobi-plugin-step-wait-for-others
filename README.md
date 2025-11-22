e479d2fa3f923e15393e47f4f69ef9ddfacb2bb0
Steffen Hankiewicz <steffen.hankiewicz@intranda.com>
2025-11-22 17:33:12 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Goobi workflow Plugin: goobi-plugin-step-kick-start
+# Goobi workflow Plugin: goobi-plugin-step-wait-for-others
 
 <img src="https://goobi.io/wp-content/uploads/logo_goobi_plugin.png" align="right" style="margin:0 0 20px 20px;" alt="Plugin for Goobi workflow" width="175" height="109">
 
-{KICKSTART_DESCRIPTION}
+This Step Plugin for Goobi workflow allows to wait for other Goobi processes with the same property to reach the same status before all of them are move forward aagin. Optionally they can be collected into the same batch that can be created.
 
 This is a plugin for Goobi workflow, the open source workflow tracking software for digitisation projects. More information about Goobi workflow is available under https://goobi.io. If you want to get in touch with the user community simply go to https://community.goobi.io.
 
@@ -13,7 +13,7 @@ More information about the functionality of this plugin and the complete documen
 
 Detail | Description
 --- | ---
-**Plugin identifier**       | intranda_step_kick_start
+**Plugin identifier**       | intranda_step_wait_for_others
 **Plugin type**             | Step plugin
 **Licence**                 | GPL 2.0 or newer    
 **Documentation (German)**  | - no documentation available -
--- module-base/src/test/java/de/intranda/goobi/plugins/WaitForOthersPluginTest.java
+++ module-base/src/test/java/de/intranda/goobi/plugins/WaitForOthersPluginTest.java
@@ -43,7 +43,7 @@ import ugh.fileformats.mets.MetsMods;
 @PrepareForTest({ MetadatenHelper.class, VariableReplacer.class, ConfigurationHelper.class, ProcessManager.class,
         MetadataManager.class })
 @PowerMockIgnore({ "javax.management.*", "javax.xml.*", "org.xml.*", "org.w3c.*", "javax.net.ssl.*", "jdk.internal.reflect.*" })
-public class KickStartPluginTest {
+public class WaitForOthersPluginTest {
 
     private static String resourcesFolder;
 
@@ -71,13 +71,13 @@ public class KickStartPluginTest {
 
     @Test
     public void testConstructor() throws Exception {
-        KickStartStepPlugin plugin = new KickStartStepPlugin();
+        WaitForOthersStepPlugin plugin = new WaitForOthersStepPlugin();
         assertNotNull(plugin);
     }
 
     @Test
     public void testInit() {
-        KickStartStepPlugin plugin = new KickStartStepPlugin();
+        WaitForOthersStepPlugin plugin = new WaitForOthersStepPlugin();
         plugin.initialize(step, "something");
         assertEquals(step.getTitel(), plugin.getStep().getTitel());
     }
@@ -154,7 +154,7 @@ public class KickStartPluginTest {
 
     public Process getProcess() {
         Project project = new Project();
-        project.setTitel("KickStartProject");
+        project.setTitel("WaitForOthersProject");
 
         Process process = new Process();
         process.setTitel("00469418X");
