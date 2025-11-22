7f76c626cb405e900658d509ce57218a59a84931
Steffen Hankiewicz <steffen.hankiewicz@intranda.com>
2025-11-22 20:20:55 +0100

    first working version

--- module-base/src/main/java/de/intranda/goobi/plugins/WaitForOthersStepPlugin.java
+++ module-base/src/main/java/de/intranda/goobi/plugins/WaitForOthersStepPlugin.java
@@ -1,5 +1,8 @@
 package de.intranda.goobi.plugins;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * This file is part of a plugin for Goobi - a Workflow tool for the support of mass digitization.
  *
@@ -20,19 +23,30 @@ package de.intranda.goobi.plugins;
  */
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.apache.commons.configuration.SubnodeConfiguration;
+import org.goobi.beans.Batch;
+import org.goobi.beans.GoobiProperty;
+import org.goobi.beans.JournalEntry;
+import org.goobi.beans.JournalEntry.EntryType;
 import org.goobi.beans.Step;
+import org.goobi.production.enums.LogType;
 import org.goobi.production.enums.PluginGuiType;
 import org.goobi.production.enums.PluginReturnValue;
 import org.goobi.production.enums.PluginType;
 import org.goobi.production.enums.StepReturnValue;
+import org.goobi.production.flow.statistics.hibernate.FilterHelper;
 import org.goobi.production.plugin.interfaces.IStepPluginVersion2;
 
 import de.sub.goobi.config.ConfigPlugins;
+import de.sub.goobi.helper.HelperSchritte;
+import de.sub.goobi.persistence.managers.JournalManager;
+import de.sub.goobi.persistence.managers.ProcessManager;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
+
 
 @PluginImplementation
 @Log4j2
@@ -42,12 +56,10 @@ public class WaitForOthersStepPlugin implements IStepPluginVersion2 {
     private String title = "intranda_step_wait_for_others";
     @Getter
     private Step step;
-    @Getter
-    private String value;
-    @Getter 
-    private boolean allowTaskFinishButtons;
     private String returnPath;
-
+    private String property;
+    private boolean createBatch = false;
+    
     @Override
     public void initialize(Step step, String returnPath) {
         this.returnPath = returnPath;
@@ -55,17 +67,14 @@ public class WaitForOthersStepPlugin implements IStepPluginVersion2 {
                 
         // read parameters from correct block in configuration file
         SubnodeConfiguration myconfig = ConfigPlugins.getProjectAndStepConfig(title, step);
-        value = myconfig.getString("value", "default value"); 
-        allowTaskFinishButtons = myconfig.getBoolean("allowTaskFinishButtons", false);
+        property = myconfig.getString("property", "default property"); 
+        createBatch = myconfig.getBoolean("createBatch", false);
         log.info("WaitForOthers step plugin initialized");
     }
 
     @Override
     public PluginGuiType getPluginGuiType() {
-        return PluginGuiType.FULL;
-        // return PluginGuiType.PART;
-        // return PluginGuiType.PART_AND_FULL;
-        // return PluginGuiType.NONE;
+    	return PluginGuiType.NONE;
     }
 
     @Override
@@ -107,7 +116,75 @@ public class WaitForOthersStepPlugin implements IStepPluginVersion2 {
     @Override
     public PluginReturnValue run() {
         boolean successful = true;
-        // your logic goes here
+        
+        // get the value of the property
+        String propValue = null;
+        for (GoobiProperty prop : step.getProzess().getEigenschaften()) {
+			if (prop.getPropertyName().equals(property)) {
+				propValue = prop.getPropertyValue();
+				break;
+			}
+		}
+        
+        // if value is null throw error
+        if (propValue==null) {
+        	log.error("Property " + propValue + " has no value to check");
+        	JournalEntry entry = new JournalEntry(step.getProzess().getId(), new Date(), "Plugin " + title, LogType.ERROR,
+        			"Property " + propValue + " has no value to check.", EntryType.PROCESS);
+    		JournalManager.saveJournalEntry(entry);
+        	return PluginReturnValue.ERROR;
+        }
+        
+        List<Step> stepsToClose = new ArrayList<>();
+        String sql = FilterHelper.criteriaBuilder("\"processproperty:" + property + ":" + propValue + "\"", false, null, null, null, true, false);
+        List<org.goobi.beans.Process> processes = ProcessManager.getProcesses("prozesse.titel", sql, null);
+        for (org.goobi.beans.Process p : processes) {
+            if (p.getId().equals(step.getProcessId())) {
+                // skip current process
+                continue;
+            }
+            
+            // check if all other processes reached this step
+            for (Step otherstep : p.getSchritte()) {
+                if (step.getTitel().equals(otherstep.getTitel())) {
+                    // found step to check
+                    switch (otherstep.getBearbeitungsstatusEnum()) {
+                        case LOCKED:
+                            // if no - wait
+                            return PluginReturnValue.WAIT;
+                        case ERROR:
+                        case INFLIGHT:
+                        case INWORK:
+                        case OPEN:
+                            // must be closed at the end
+                            stepsToClose.add(otherstep);
+                            break;
+                        case DEACTIVATED:
+                        case DONE:
+                        default:
+                            // already got further, do nothing
+                            break;
+
+                    }
+                }
+            }
+        }
+        
+        // we reached this, so we don't have any locked steps
+        Batch newBatch = new Batch();
+        newBatch.setBatchName(property + " " + propValue);
+        step.getProzess().setBatch(newBatch);
+        ProcessManager.saveProcessInformation(step.getProzess());
+        
+        // close the step in all other processes
+        if (!stepsToClose.isEmpty()) {
+            HelperSchritte hs = new HelperSchritte();
+            for (Step other : stepsToClose) {
+            	other.getProzess().setBatch(newBatch);
+            	ProcessManager.saveProcessInformation(other.getProzess());
+                hs.CloseStepObjectAutomatic(other);
+            }
+        }
         
         log.info("WaitForOthers step plugin executed");
         if (!successful) {
